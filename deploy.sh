2e9028b312ac386a60f0c1fd8c7786a7d353b21c
iustron@naver.com <iustron@naver.com>
2020-10-31 19:38:06 +0900

    [Test]

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cd /home/ec2-user/deploy
+cd /home/ubuntu/deploy
 
-/home/ec2-user/.nvm/versions/node/v15.0.1/bin/npm install
+npm install
 
-/home/ec2-user/.nvm/versions/node/v15.0.1/bin/pm2 start app.js
+pm2 start app.js
